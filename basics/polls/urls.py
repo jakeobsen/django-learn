526adca6734a068d8a0bc265e5046eb9ade220ba
jakeobsen <morten@jakeobsen.com>
2020-03-05 22:01:57 +0100

    Add kate view

--- basics/polls/urls.py
+++ basics/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('kate/', views.kate, name='kate'),
     path('', views.index, name='index'),
     path('bob/', views.bob, name='bob'),
 ]
--- basics/polls/views.py
+++ basics/polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def kate(request):
+    return HttpResponse("This is kate.")
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
